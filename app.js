627d1077d34e69ef6ab6956498ba8a18aa69c47d
stefanbanu <stefanbanu1@gmail.com>
2020-01-25 04:26:25 +0200

    temperature converter

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-window.onload = function () {
-    let counter = 1;
-    document.getElementById("btn").addEventListener('click', function () {
-        document.getElementById("app").innerHTML = counter++;
-    });
-};
+function fromCelsiusToFahrenheit() {
+  console.log("test");
+  let celsius = document.getElementById("celsius").value;
+  document.getElementById("fahr").value = celsius * (9 / 5) + 32;
+}
+
+function fromFahrenheitToCelsius() {
+  let fahr = document.getElementById("fahr").value;
+  document.getElementById("celsius").value = (fahr - 32) * (5 / 9);
+}
+
+function reset() {
+  document.getElementById("fahr").value = "";
+  document.getElementById("celsius").value = "";
+}
