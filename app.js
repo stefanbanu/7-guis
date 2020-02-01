c89a03422e601bf1f6a46d55bf719ed5d9baf81d
stefanbanu <stefanbanu1@gmail.com>
2020-02-01 18:49:33 +0200

    refactored

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-window.onload = function () {
-    let counter = 1;
-    document.getElementById("btn").addEventListener('click', function () {
-        document.getElementById("app").innerHTML = counter++;
-    });
-};
+let counter = 1;
+const content = document.getElementById("app");
+
+document.getElementById("btn").addEventListener("click", function() {
+  content.innerHTML = counter++;
+});
